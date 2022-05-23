535786988913d58ee0a72abed02ead652d3a54d7
Valeramadriz <valeramadriz@gmail.com>
2022-05-22 23:46:25 -0300

    Subida de archivos con JS

---
+++ JS/funtions.js
@@ -0,0 +1,19 @@
+const porcentaje = {
+    "1": 0.2,
+    "2": 0.5,
+    "3": 0.85
+};
+
+function Resumen() {
+    const form = document.getElementById("tickets");
+    const cantidad = form.elements["cant"];
+    const categoria = form.elements["cat"];
+    const sCat = categoria.options[categoria.selectedIndex];
+    let total = form.elements["total"];
+    total.value = "Total a pagar: $" + (cantidad.value * 200 * Precio[sCat.value]);
+}
+
+function resetForm() {
+    const form = document.getElementById("tickets");
+    form.reset();
+}
